259ec1f65a32194d5920cab7d7f98f5b82a0bf4d
kyrie <13148656108@163.com>
2023-02-19 21:31:08 +0800

    fix: ts

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="hello: docker + jenkins + vue3" />
+  <HelloWorld msg="hello: docker + jenkins + vue3 + ts" />
 </template>
 
 <style scoped>
